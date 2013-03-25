434d6bed7afa13e95a1df16f336871092f605e1f
Sandy <santhai.phan@gmail.com>
2013-03-25 15:00:41 +1100

    Lab02 programming task - implemented a pingClient. (resources from Internet).

---
+++ PingClient/src/PingClient.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+
+
+public class PingClient {
+	private static final int TIME_OUT = 1000; // milliseconds
+	
+	public static void main(String args[]) throws Exception {
+		if (args.length < 2) {
+			System.out.println("Required arguements: host, port");
+			return;
+		}
+		
+		// The port number to listen to.
+		int port = Integer.parseInt(args[1]);
+		
+		// The server to connect to.
+		InetAddress server;
+		server = InetAddress.getByName(args[0]);
+		
+		// Create a datagram socket for sending and receiving UDP
+		// packets through the port specified on the command line.
+		DatagramSocket socket = new DatagramSocket();
+		
+		int sequence_number;
+		
+		// Processing loop
+		for (sequence_number = 0; sequence_number < 10; sequence_number++) {
+			// Get timestamp
+			Date currentTime = new Date();
+			long msSend = currentTime.getTime();
+			
+			// Create string to send, and transfer i to a Byte Array
+			String str = "PING " + sequence_number + " " + msSend + " \n";
+			byte[] buffer = str.getBytes();
+			
+			// Create a Ping datagram to the specified server
+			DatagramPacket ping = new DatagramPacket(buffer, buffer.length, server, port);
+			
+			// Send the Ping data packet to the specified server
+			socket.send(ping);
+			
+			// Try to receive the packet
+			// Fail when timeout
+			try {
+				// Set the timeout to 1000ms = 1 sec specified
+				socket.setSoTimeout(TIME_OUT);
+				
+				// Create a datagram packet to hold incoming UDP packet.
+		        DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+		        
+		        // Try to receive the response from the server
+		        socket.receive(response);
+		        
+		        // Timestamp for the receive time
+		        currentTime = new Date();
+		        long msReceived = currentTime.getTime();
+		        
+		        // Print the packet and the delay
+		        printData(response, msReceived - msSend);
+			} catch (IOException e) {
+				// Print which packet has timeout
+				System.out.println("Timeout for packet " + sequence_number);
+			}
+		}
+	}
+		
+	/* 
+	 * Print ping data to the standard output stream.
+	 * slightly changed from PingServer
+	 */
+	private static void printData(DatagramPacket request, long delayTime) throws Exception{
+		// Obtain references to the packet's array of bytes.
+		byte[] buf = request.getData();
+		
+		// Wrap the bytes in a byte array input stream,
+		// so that you can read the data as a stream of bytes.
+		ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+		
+		// Wrap the byte array output stream in an input stream reader,
+		// so you can read the data as a stream of characters.
+		InputStreamReader isr = new InputStreamReader(bais);
+		
+		// Wrap the input stream reader in a buffered reader,
+		// so you can read the character data a line at a time.
+		// (A line is a sequence of chars terminated by any combination of \r and \n.) 
+		BufferedReader br = new BufferedReader(isr);
+		
+		// The message data is contained in a single line, so read this line.
+		String line = br.readLine();
+		
+		// Print host address and data received from it.
+		System.out.println(
+		"Received from " + 
+		request.getAddress().getHostAddress() + 
+		": " +
+		new String(line) + " Delay: " + delayTime );
+	}
+}
