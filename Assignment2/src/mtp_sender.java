b567c9016dbac6d588022d5dbcec8ab0b94a9aed
sandy-ubuntu <santhai.phan@gmail.com>
2013-06-04 00:12:00 +1000

    not finished.

--- Assignment2/src/mtp_sender.java
+++ Assignment2/src/mtp_sender.java
@@ -1,7 +1,9 @@
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -21,57 +23,109 @@ public class mtp_sender {
 		int mws = Integer.parseInt(args[3]);
 		int mss = Integer.parseInt(args[4]);
 		int timeout = Integer.parseInt(args[5]);
+		int seqNum = 0;
+		boolean finished = false;
 		
 		// calculate the number of packets can be
 		// sent at once.
 		int packetNum = mws / mss;
 		
 		DatagramSocket socket = new DatagramSocket();
-		FileInputStream fis = new FileInputStream(new File(fileName));
+		socket.setSoTimeout(timeout);
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		
+		if (!file.exists())
+			file.createNewFile();
 		
 		try {
 			byte[] buff = new byte[mss];
 			int remaining = buff.length;
-			while (true) {
-//				int read = fis.read(buff, buff.length - remaining, remaining);
-//				if (read > 0) {
-//					remaining -= read;
-//					if (remaining == 0) {
-//						System.out.println("sending data");
-//						DatagramPacket packet = new DatagramPacket(buff, buff.length, server, receiverPort);
-//						socket.send(packet);
-////						System.out.println(new String(packet.getData()));
-//						remaining = buff.length;
-//					}
-//				}
+			while (!finished) {
+				if (establishConnection(socket, server, receiverPort)) {
+					Package p;
+					int read = fis.read(buff, buff.length - remaining, remaining);
+					p = new Package(seqNum);
+					if (read >= 0) {
+						remaining -= read;
+						if (remaining == 0) { 
+							remaining = buff.length;
+							seqNum += read;
+						} else {
+							seqNum += read;
+							finished = true;
+						}
+					}
+					
+					p.setData(buff);
+					p.setSeqNumber(seqNum);
+					DatagramPacket pt = new DatagramPacket(buff, buff.length, server, receiverPort);
+					System.out.println(new String(new String(p.getData().toString())));
+					ByteArrayOutputStream bos = new ByteArrayOutputStream();
+					ObjectOutputStream out = new ObjectOutputStream(bos);
+					out.writeObject(p);
+					DatagramPacket packet = new DatagramPacket(bos.toByteArray(), bos.size(), server, receiverPort);
+					socket.send(packet);
+					System.out.println("Packet segment sent");
+				}
 				
-//				try {
-//					socket.setSoTimeout(timeout);
-//					
-//					DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
-//					
-//					socket.receive(response);
-//					System.out.println("Received response");
-//				} catch (IOException e) {
-//					System.out.println("Timeout");
-//				}
+				try {
+					socket.setSoTimeout(timeout);
+					
+					DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+					
+					socket.receive(response);
+					System.out.println("Received response");
+				} catch (IOException e) {
+					System.out.println("Timeout");
+				}
 			}
 		} finally {
 			fis.close();
 		}
 	}
 	
-	public boolean establishConnection(DatagramSocket socket) throws Exception {
-		Package establishment = new Package(0);
+	public static boolean establishConnection(DatagramSocket socket, InetAddress server, int port) throws Exception {
+		Package establishment = new Package(CLIENT_ISN);
 		establishment.setSYN(true);
+//		DatagramPacket packet = new DatagramPacket(buff, buff.length, server, port);
 		
 		while (!connectionEstalished) {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream(2048);
+			System.out.println("Establishing connection:");
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			ObjectOutputStream out = new ObjectOutputStream(bos);
 			out.writeObject(establishment);
+			
+			DatagramPacket packet = new DatagramPacket(bos.toByteArray(), bos.size(), server, port);
+			socket.send(packet);
+			System.out.println("Establishment written");
+			
+			try {
+				DatagramPacket response = new DatagramPacket(new byte[FILE_SIZE], FILE_SIZE);
+				socket.receive(response);
+				System.out.print(response.getData());
+				System.out.println("Establishment packet: " + establishment);
+				ByteArrayInputStream bis = new ByteArrayInputStream(response.getData());
+				ObjectInputStream in = new ObjectInputStream(bis);
+				System.out.println(in);
+				Package p = (Package) in.readObject();
+				
+				if (p.isACK() && p.isSYN()) {
+					System.out.println("SYNACK received");
+					connectionEstalished = true;
+					
+					establishment.setSeqNumber(establishment.getSeqNumber() + 1);
+					establishment.setAckNumber(p.getSeqNumber() + 1);
+					out.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		return false;
+		return true;
 	}
 	
 	private static boolean connectionEstalished = false;
+	private static final int FILE_SIZE = 1024 * 5;
+	private static final int CLIENT_ISN = 3;
 }
--- Assignment2/src/mtp_server.java
+++ Assignment2/src/mtp_server.java
@@ -1,7 +1,13 @@
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 
 public class mtp_server {
@@ -13,21 +19,57 @@ public class mtp_server {
 		
 		int myPort = Integer.parseInt(args[0]);
 		String fileName = args[1];
+		int clientPort;
+		InetAddress clientAddress;
 		
 		DatagramSocket socket = new DatagramSocket(myPort);
+		File file = new File(fileName);
+		FileOutputStream fos = new FileOutputStream(file);
+		
+		if (!file.exists())
+			file.createNewFile();
 		
 		while (true) {
-//			DatagramPacket request = new DatagramPacket(new byte[FILE_SIZE], FILE_SIZE);
-//			socket.receive(request);
-//			System.out.println("Received data");
-//			System.out.println(new String(request.getData()));
+			DatagramPacket request = new DatagramPacket(new byte[FILE_SIZE], FILE_SIZE);
+			socket.receive(request);
+			System.out.println("Received data");
+			System.out.println(new String(request.getData()));
+//			System.out.println(request.getLength());
 			
-			ByteArrayInputStream bis = new ByteArrayInputStream(new byte[FILE_SIZE]);
+			ByteArrayInputStream bis = new ByteArrayInputStream(request.getData());
 			ObjectInputStream in = new ObjectInputStream(bis);
 			Package p = (Package) in.readObject();
-			System.out.println(p.isSYN());
+//				System.out.println(p.isSYN());
+			System.out.println(p.getSeqNumber());
+			
+			Package reply;
+			if (p.isSYN()) {
+				System.out.println("SYN packet received");
+				reply = new Package(SERVER_ISN);
+				reply.setACK(true);
+				reply.setSYN(true);
+				reply.setAckNumber(p.getSeqNumber() + 1);
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				ObjectOutputStream out = new ObjectOutputStream(bos);
+				out.writeObject(reply);
+				out.close();
+				
+				clientAddress = request.getAddress();
+				clientPort = request.getPort();
+				
+				DatagramPacket response = new DatagramPacket(bos.toByteArray(), bos.size(), clientAddress, clientPort);
+				socket.send(response);
+				System.out.println("Response sent");
+			} else {
+				if (p.getData().length > 0) {
+					fos.write(p.getData());
+					fos.flush();
+				}
+			}
 		}
+		
 	}
 	
 	private static final int FILE_SIZE = 1024 * 5;
+	private static final int SERVER_ISN = 6;
 }
